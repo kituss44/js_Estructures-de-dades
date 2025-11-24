323055446db59913d3041f3d2e2c71be2b7f96f7
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-24 12:06:22 +0100

    exercici_1

--- js_estructures_de_dades.js
+++ js_estructures_de_dades.js
@@ -1,1 +1,15 @@
 // Exercici 1:
+
+let nums = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100));
+
+nums.sort((a, b) => a - b);
+
+let primer = nums[0];
+let ultim = nums[nums.length - 1];
+
+console.log("Array ordenat:", nums);
+console.log("Primer:", primer);
+console.log("Últim:", ultim);
+
+// Exercici 2:.
+
